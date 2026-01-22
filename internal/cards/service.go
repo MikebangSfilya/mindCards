b55f6c0286e9a5fd08c23a11b5b92e061a335fcb
Sfilya <Typegoot@gmail.com>
2026-01-22 21:31:27 +0300

    add all cached methods

--- internal/cards/service.go
+++ internal/cards/service.go
@@ -2,6 +2,7 @@ package cards
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"time"
@@ -93,6 +94,8 @@ func (s *Service) AddCards(ctx context.Context, userId int, cardParams []Card) (
 
 // DeleteCard card from DB
 func (s *Service) DeleteCard(ctx context.Context, cardId, userId int) error {
+	key := fmt.Sprintf("cards:u:%d:c:%d", userId, cardId)
+
 	err := s.txManager.WithinTransaction(ctx, func(ctx context.Context, q DBQuerier) error {
 		if err := s.Repo.DeleteCard(ctx, q, cardId, userId); err != nil {
 			return err
@@ -103,24 +106,25 @@ func (s *Service) DeleteCard(ctx context.Context, cardId, userId int) error {
 		return fmt.Errorf("batch delete cards failed: %w", err)
 	}
 
+	_ = s.Redis.Delete(ctx, key)
 	s.logger.Info("batch delete completed", "cardId", cardId)
 	return nil
 }
 
 // UpdateCardDescription new description in DB
 func (s *Service) UpdateCardDescription(ctx context.Context, cardId, UserID int, cardsUp Update) (*MindCard, error) {
+	key := fmt.Sprintf("cards:u:%d:c:%d", UserID, cardId)
+
 	row, err := s.Repo.UpdateCardDescription(ctx, nil, cardId, UserID, cardsUp.NewDescription)
 	if err != nil {
 		return nil, fmt.Errorf("update card description: %w", err)
 	}
 
 	s.logger.Info("update completed", "cardId", cardId)
+	if err := s.Redis.Delete(ctx, key); err != nil {
+		return nil, fmt.Errorf("redis delete: %w", err)
+	}
 	return rowToCard(row), nil
-}
-
-// UpdateLvl Возможно не понадобится
-func (s *Service) UpdateLvl() {
-
 }
 
 // Getcards list of cards
@@ -128,8 +132,7 @@ func (s *Service) GetCards(ctx context.Context, userId int, limit, offset int16)
 	key := fmt.Sprintf("cards:u:%d:l:%d:o:%d", userId, limit, offset)
 
 	var cachedCards []*MindCard
-	err := s.Redis.Get(ctx, key, &cachedCards)
-	if err == nil {
+	if s.getFromCache(ctx, key, &cachedCards) {
 		return cachedCards, nil
 	}
 
@@ -150,13 +153,30 @@ func (s *Service) GetCards(ctx context.Context, userId int, limit, offset int16)
 
 // Get cards filtered by Tag
 func (s *Service) GetCardsByTag(ctx context.Context, tag string, userId int, limit, offset int16) ([]*MindCard, error) {
+	key := fmt.Sprintf("cards:u:%dt:%s:l:%d:o:%d", userId, tag, limit, offset)
+
+	var cachedCards []*MindCard
+	if s.getFromCache(ctx, key, &cachedCards) {
+		return cachedCards, nil
+	}
 
 	rows, err := s.Repo.GetCardsByTag(ctx, nil, tag, userId, limit, offset)
 	if err != nil {
 		return nil, err
 	}
 
-	return rowsToCards(rows), nil
+	cards := rowsToCards(rows)
+
+	go func() {
+		setCtx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		if err := s.Redis.Set(setCtx, key, cards, 1*time.Minute); err != nil {
+			s.logger.Warn("redis set failed", "err", err)
+		}
+	}()
+
+	return cards, nil
 }
 
 // Get one card by ID
@@ -164,8 +184,7 @@ func (s *Service) GetCardById(ctx context.Context, cardID, userID int) (*MindCar
 	key := fmt.Sprintf("cards:u:%d:c:%d", userID, cardID)
 
 	var card MindCard
-	err := s.Redis.Get(ctx, key, &card)
-	if err == nil {
+	if s.getFromCache(ctx, key, &card) {
 		return &card, nil
 	}
 
@@ -213,5 +232,18 @@ func rowsToCards(rows []storage.CardRow) []*MindCard {
 	}
 
 	return result
+}
 
+func (s *Service) getFromCache(ctx context.Context, key string, dest any) bool {
+	err := s.Redis.Get(ctx, key, dest)
+	if err == nil {
+		return true
+	}
+	if errors.Is(err, redis2.ErrCacheMiss) {
+		return false
+	}
+	s.logger.Warn("cache failure, clearing key", "key", key, "error", err)
+
+	_ = s.Redis.Delete(ctx, key)
+	return false
 }
